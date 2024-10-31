2c305e1f053e724828dec3519b615b7194f283ae
Luke Farrell <lukefarrell2012@gmail.com>
2024-10-31 17:44:13 +0000

    small styling choices

--- KanbanBoard/src/components/Navbar.jsx
+++ KanbanBoard/src/components/Navbar.jsx
@@ -4,7 +4,6 @@ export default function Navbar() {
   return (
     <div id="navbar-container">
       <img id="website-logo" src={logoImage} alt="Logo Image" />
-      <h1 id="website-name">TaskCanvas</h1>
     </div>
   );
 }
