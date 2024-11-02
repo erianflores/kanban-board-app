7388f22bd964e852148bb7de5dd7ae00422ae4a6
Erian Flores <erian.flores@gmail.com>
2024-11-02 16:03:40 +0100

    updated

--- KanbanBoard/src/App.jsx
+++ KanbanBoard/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import KanbanBoard from './components/KanbanBoard';
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import KanbanBoard from "./components/KanbanBoard";
+import { Routes, Route } from "react-router-dom";
+import AboutPage from "./components/AboutPage";
+import Details from "./components/Details";
 
 export default function App() {
-
-
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<KanbanBoard />} />
+        <Route path="/about-page" element={<AboutPage />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
       <Sidebar />
       <KanbanBoard />
       <Footer />
     </>
-  )
+  );
 }
